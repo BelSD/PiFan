9c77a3292a9e68d65f2a6cf336574a9ac15fcade
BelSD <jpw@belsd.be>
2021-01-16 15:14:43 +0100

    Divers améliorations:
    - Contrôle si fichier source ne change pas et si oui, alors restart du programme
    - Démarrage direct du FAN au démarrage et cela minimum pour 5 secondes

--- BelSD_PiFan.py
+++ BelSD_PiFan.py
@@ -4,18 +4,28 @@
 #
 # Raspberry Temperature PiFan Control
 # 
-# Version : 0.2.0
+# Version : 1.0
 #
-
+import os
+import sys
+from os.path import getmtime
 from time import sleep
 from gpiozero import OutputDevice
 from termcolor import colored, cprint
 
 STARTING = True
+LOOP = True
 FAN_ON = 65   # Température d'activation du/des ventilateur(s) (en degrés Celcius).
 FAN_OFF = 55  # Température de déactivation du/des ventilateur(s) (en degrés Celcius).
-WAIT = 5      # Fréquence de mesure de la température (en secondes).
 GPIO_PIN = 14 # GPIO où est branché le contrôleur du/des ventilateur(s)
+
+fan = OutputDevice(GPIO_PIN)
+fan.on()
+
+# Info sur le chier actuel pour voir si une modification du code est présente
+# et le cas échéant, redémarrer le programme pour la prise en considération du 
+# nouveau code source
+WATCHED_FILE_TIME = getmtime(__file__)
 
 def read_temp():
     # 
@@ -30,23 +40,34 @@ def read_temp():
         raise RuntimeError('Impossible d\'analyser la sortie de température.') from ERROR_STR
 
 if __name__ == '__main__':
-    if STARTING == True:
-        print ('[', colored('  OK  ', 'green'),']', ' Started', colored(' BelSD PiFan Control.', 'white'))
+
     # Valider les seuils d'activation et de désactivation
     if FAN_OFF >= FAN_ON:
-        raise RuntimeError('FAN_OFF doit-être inférieur à FAN_ON')
+        print ('[', colored(' KO ', 'red'),']', 'Started', colored('BelSD PiFan Control.', 'white'))
+        print ('RuntileError: FAN_OFF doit-être inférieur à FAN_ON')
+        LOOP = False
 
-    fan = OutputDevice(GPIO_PIN)
+    while LOOP:
+        # Check si le code source a changé, si oui, on redémare le programme
+        # pour la prise en considération du nouveau code
+        if getmtime(__file__) != WATCHED_FILE_TIME:
+            print (colored('BelSD PiFan Control --> ', 'white'), colored('Restart', 'yellow'))
+            os.execv(sys.executable, ['python3'] + sys.argv)
 
-    while True:
-        CPU_TEMPERATURE = read_temp()
-        # Active le(s) ventilateur(s) si le seuil de température est atteint
-        # et qu'il(s) n'est/ne sont pas déjà activé(s)
-        # NOTE: `fan.value` retourne 1 pour "on" et 0 pour "off"
-        if CPU_TEMPERATURE > FAN_ON and not fan.value:
-            fan.on()
-        # Arrête le(s) ventilateur(s) si la température est en dessous su seuil de température
-        elif fan.value and CPU_TEMPERATURE < FAN_OFF:
-            fan.off()
+        if STARTING == True:
+            STARTING = False
+            print ('[', colored(' OK ', 'green'),']', 'Started', colored('BelSD PiFan Control.', 'white'))        
+        else:
+            CPU_TEMPERATURE = read_temp()
+            # Active le(s) ventilateur(s) si le seuil de température est atteint
+            # et qu'il(s) n'est/ne sont pas déjà activé(s)
+            # NOTE: `fan.value` retourne 1 pour "on" et 0 pour "off"
+            if CPU_TEMPERATURE > FAN_ON and not fan.value:
+                fan.on()
+            # Arrête le(s) ventilateur(s) si la température est en dessous su seuil de température
+            elif fan.value and CPU_TEMPERATURE < FAN_OFF:
+                fan.off()
 
-        sleep(WAIT)
+        sleep(5)
+    
+    fan.off()
