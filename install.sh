93a2f5566a7c35deb2e82de664ec70504f030163
BelSD <jpw@belsd.be>
2021-01-17 17:35:07 +0100

    Modif install dir

--- install.sh
+++ install.sh
@@ -3,17 +3,15 @@
 
 set -e
 
-#cd "$(dirname "$0")/.."
-
 sudo apt-get -y update
 sudo apt-get install python3-gpiozero -y
 
 echo "=> Installation de BelSD PiFan Controller...\n"
-sudo cp BelSD_PiFan.py /usr/local/bin/
+sudo cp Pifan/BelSD_PiFan.py /usr/local/bin/
 sudo chmod +x /usr/local/bin/BelSD_PiFan.py
 
 echo "=> Démarrage de BelSD PiFan Controller...\n"
-sudo cp BelSD_PiFan.sh /etc/init.d/
+sudo cp Pifan/BelSD_PiFan.sh /etc/init.d/
 sudo chmod +x /etc/init.d/BelSD_PiFan.sh
 
 sudo update-rc.d BelSD_PiFan.sh defaults
